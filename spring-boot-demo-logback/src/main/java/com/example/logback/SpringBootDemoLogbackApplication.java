03f673032db0d0308e6d3a81a3183ce36f481c4c
fengdan <fengdan80@outlook.com>
2022-08-08 17:22:52 +0800

    Spring Boot Demo Logback

---
+++ spring-boot-demo-logback/src/main/java/com/example/logback/SpringBootDemoLogbackApplication.java
@@ -0,0 +1,16 @@
+package com.example.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author springboot
+ */
+@SpringBootApplication
+public class SpringBootDemoLogbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoLogbackApplication.class, args);
+	}
+
+}
---
+++ spring-boot-demo-logback/src/main/java/com/example/logback/controller/LogbackController.java
@@ -0,0 +1,22 @@
+package com.example.logback.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Slf4j
+@ResponseBody
+@Controller
+public class LogbackController {
+
+    @GetMapping("/log")
+    private void logTest() {
+        log.trace("trace");
+        log.debug("debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
+    }
+}
