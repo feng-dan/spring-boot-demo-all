319404e078bdc0068a63b56966a2674bda6d5ac2
fengdan <fengdan80@outlook.com>
2022-09-06 12:02:26 +0800

    add Spring Boot Demo Web

---
+++ spring-boot-demo-web/src/main/java/com/example/springbootdemoweb/SpringBootDemoWebApplication.java
@@ -0,0 +1,25 @@
+package com.example.springbootdemoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author heipai
+ */
+@SpringBootApplication
+@RestController
+@RequestMapping("/spring-boot-web")
+public class SpringBootDemoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoWebApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return " hello spring boot web";
+    }
+}
